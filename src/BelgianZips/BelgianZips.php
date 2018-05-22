b1ba00b89a865cc3767f6936f7799b5ef679236f
Jan Decavele <jandecavele@gmail.com>
2018-05-22 13:13:34 +0200

    Provide optional caching

--- src/BelgianZips/BelgianZips.php
+++ src/BelgianZips/BelgianZips.php
@@ -3,14 +3,34 @@
 
 namespace BelgianZips;
 
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\Common\Cache\Cache;
+
 class BelgianZips
 {
-    /** @var array */
-    private $zipcodes;
+    const CACHE_IDENTIFIER = 'belgianzips.zipcodes';
 
-    public function __construct()
+
+    /** @var array */
+    private $zipCodes;
+
+    /** @var ArrayCache */
+    private $cache;
+
+    public function __construct(Cache $cache = null)
     {
-        $this->zipcodes = json_decode(file_get_contents(__DIR__ . '/../../var/zipcodes.json'), true);
+        $this->cache = $cache;
+
+        if (is_null($cache)) {
+            $this->cache = new ArrayCache();
+        }
+
+        if (!$this->cache->contains(self::CACHE_IDENTIFIER)) {
+            $zipcodes = json_decode(file_get_contents(__DIR__ . '/../../var/zipcodes.json'), true);
+            $this->cache->save(self::CACHE_IDENTIFIER, $zipcodes);
+        }
+
+        $this->zipCodes = $this->cache->fetch(self::CACHE_IDENTIFIER);
     }
 
     /**
@@ -20,7 +40,7 @@ class BelgianZips
      */
     private function filterZipcodes(array $conditions = [])
     {
-        return array_filter($this->zipcodes, function (array $instance) use ($conditions) {
+        return array_filter($this->zipCodes, function (array $instance) use ($conditions) {
 
             foreach ($conditions as $key => $condition) {
                 if (strtolower($instance[$key]) != strtolower($condition)) { // Type/case juggling
